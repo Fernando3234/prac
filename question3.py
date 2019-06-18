47968d15a5139af66a2d00936bdb6154b8f0d3e9
Fernando <Fernando32345@gmail.com>
2019-06-17 23:28:58 -0400

    third question

---
+++ question3.py
@@ -0,0 +1,8 @@
+#This program will prompt for a quote and a author then will display author and quote
+
+#Initialize Variable and input
+quote = input("Pick a quote?")
+author = input("Who said that Quote?")
+
+#display quote and Author
+print("%s says %s" % (author, quote) )
