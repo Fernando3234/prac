92b67c125d06dca7b292df2081e1b540b4e0576b
Fernando <Fernando32345@gmail.com>
2019-06-17 23:58:10 -0400

    fourth question

---
+++ question4.py
@@ -0,0 +1,10 @@
+#Create Mad Lib Program
+
+#Initiate Variables
+noun = input("Input a Noun")
+verb = input ("Input a verb")
+adjective = input("input a adjective")
+adverb = input (" input a adverb")
+
+#print sentence
+print("Do you %s your %s %s %s? That's Halarious!" % (verb, adjective, noun, adverb ))
