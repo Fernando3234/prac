b0b83d00af465249f0073dc6108bafe2f55b6318
Fernando <Fernando32345@gmail.com>
2019-06-17 23:14:13 -0400

    second question

---
+++ question2.py
@@ -0,0 +1,11 @@
+# This program will prompt for a string and then it will determine how many characters long it is
+
+#Initialize Variable
+length = 0
+
+
+# user input
+text = input("Input a word: ")
+
+#present characters length to User
+print(len(text))
