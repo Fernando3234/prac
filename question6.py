b7fdffb152d46a9f8ef0068ac9b215ee7d954b1d
Fernando <Fernando32345@gmail.com>
2019-06-19 00:23:53 -0400

    sixth question v2

--- question6.py
+++ question6.py
@@ -1,16 +1,41 @@
 #Program will prompt for current age and year you can retire it should then display amount of years left for retirement
 import datetime
 
-#Initializing Variables
 currentDt = datetime.datetime.now()
 currentYear = currentDt.year
-cAge = int(input("Input your current Age"))
-rAge = int(input("Input at what age you will retire"))
 
-#Creating Retirment Calculator
+
+#Creating Fail safe
+while True:
+    print("whats your current Age")
+    input1 = input()
+    try:
+        cAge= int(input1)
+        if cAge < 0:
+            print("You can already Retire!")
+            continue
+        else:
+            break
+    except ValueError:
+        print("you need a whole number")
+
+while True:
+    print("What age do you want to retire in")
+    input2 = input()
+    try:
+        rAge= int(input2)
+        if rAge < 0:
+            print("You can already Retire!")
+            continue
+        else:
+            break
+    except ValueError:
+        print("you need a whole number")
+
+#Calculating Retirment
 years_until_retirement = (rAge - cAge)
 retirement_year = (currentYear + years_until_retirement)
 
 #presenting information to user
 print("You have %s years left until you can retire." % (years_until_retirement))
-print(" It's %s, so you can retire in %s" % (currentYear, retirement_year))
+print("It's %s, so you can retire in %s" % (currentYear, retirement_year))
