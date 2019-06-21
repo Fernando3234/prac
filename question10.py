3f948edac7367ef67751e9face596fa92d2d0c1b
Fernando <Fernando32345@gmail.com>
2019-06-21 01:39:19 -0400

    10th excercise v2

--- question10.py
+++ question10.py
@@ -1,9 +1,10 @@
 #This program is a selfcheckout which will calculate not only total but total after tax
 taxPer = 5.5
 x = False
+subTotal = 0
 #Initialize
 while True:
-    print("Please input how many items you bought ")
+    print("Please input how many items you bought")
     input1 = input()
     try:
         num = int(input1)
@@ -17,23 +18,23 @@ while True:
 
 
 #nested while loop so it can repeat teh questions depending how many items they have
-for i in range (1,int(num)+1):
-    x == False
+for i in range (num):
+    x = False
 
     while x == False:
         try:
             price = float(input("please enter the price of the item"))
             quant = float(input("please enter how many of that item do you have"))
-            if (price > 0) and (quant > 0):
-                print("Got it")
-                x = True
+            sub = (price) * (quant)
+            if (price < 0) or (quant < 0):
+                print("please input a positive number")
             else:
-                print("Please input a positive number")
+                x = True
+            subTotal = subTotal + sub
         except ValueError:
             print("please input a whole number")
 
 #Calculations
-subTotal = (price) * (quant)
 tax = subTotal * (taxPer/100)
 total = subTotal + tax
 
